5d9ae79f4b8a01bc74add285f00e86f5893ac144
Ludovico Colucci <ludovicocolucci@gmail.com>
2022-07-13 17:26:08 +0200

    add components-07

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,19 @@
 import React from "react"
 
 import { HelloWorld } from "./HelloWorld";
+import { Message } from "./Message";
 
 const helloWorldElement = <HelloWorld />
+const messageElement = <Message />
 
-export class App extends React.Component{
-    render(){
-        return <div> {helloWorldElement} </div>
+export class App extends React.Component {
+    render() {
+        return (
+        <div> 
+            {helloWorldElement}
+            {messageElement} 
+        </div>
+        )
     }
 }
 
--- my-app/src/HelloWorld.js
+++ my-app/src/HelloWorld.js
@@ -3,10 +3,7 @@ import React from "react"
 export class HelloWorld extends React.Component {
     render() {
         return (
-            <div>
                 <h1>Hello, World!</h1>
-                <p>What a beautiful day!</p>
-            </div>
             )
 
     }
