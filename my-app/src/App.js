058bff263c94dadae2fa3b6398e206b487d2a14c
Ludovico Colucci <ludovicocolucci@gmail.com>
2022-07-27 18:23:33 +0200

    fix props 03

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,7 +18,7 @@ export class App extends React.Component {
             <div>
                 {helloWorldElement}
                 {messageElement}
-                <Welcome age="21"/>
+                <Welcome age={21}/>
             </div>
         )
     }
