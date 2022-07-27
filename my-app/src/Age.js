c3b281640d479c7c4a48d41d9ccee8c312d71c08
Ludovico Colucci <ludovicocolucci@gmail.com>
2022-07-27 22:52:49 +0200

    fix props05

--- my-app/src/Age.js
+++ my-app/src/Age.js
@@ -1,9 +1,6 @@
 import React from "react";
 
-export class Age extends React.Component {
-    render() {
-        return (
-                <p>Your age is {this.props.age}</p>
-        )
-    }
+export default function Age(props) {
+    const { age } = props;
+    return <p>Your age is {age}</p>;
 }
--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Age } from "./Age";
+import Age from "./Age";
 
 
 export class Welcome extends React.Component {
