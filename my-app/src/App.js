9bff186cf93e1391a896bd51ef7adedc9fe446e1
Ludovico Colucci <ludovicocolucci@gmail.com>
2022-07-28 15:19:25 +0200

    initial commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -19,7 +19,7 @@ export class App extends React.Component {
             <div>
                 {helloWorldElement}
                 {messageElement}
-                <Welcome name={<strong>Mario</strong>} />
+                <Welcome name={<strong>Mario</strong>} age={65}/>
             </div>
         )
     }
